937d59c901ef9378663af9677c0dda08fab74aa0
Anas-kaithakoden <anaskaithakoden@gmail.com>
2025-05-19 22:16:02 +0530

    stored them in csv

--- selenium/news_handler.py
+++ selenium/news_handler.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+import pandas as pd
 website = "https://www.thesun.co.uk/sport/football/"
 path = "/home/anas/Downloads/chromedriver"
 
@@ -9,13 +9,13 @@ driver = webdriver.Chrome(service=service)
 driver.get(website)
 
 
-results = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+results = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]') # get the container
 
 titles = []
 subtitles = []
 links = []
 
-for result in results:
+for result in results: #store each data
     title = result.find_element(by="xpath", value='./a/span').text
     titles.append(title)
 
@@ -25,4 +25,8 @@ for result in results:
     link = result.find_element(by="xpath", value='/a').get_attribute("href")
     links.append[link]
 
+my_dict = {'title': titles, 'subtitle':subtitles, 'link':links}
+headlines = pd.DataFrame(my_dict)
+headlines.to_csv('headline.csv')
 
+driver.quit()
