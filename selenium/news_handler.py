ea653ab5c49db2cd02ff8b34b447cb22c7b456dc
Anas-kaithakoden <anaskaithakoden@gmail.com>
2025-05-20 10:56:08 +0530

    made the file executable

--- selenium/news_handler.py
+++ selenium/news_handler.py
@@ -2,6 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
+from datetime import datetime
+import os
+import sys
+
+if getattr(sys, 'frozen', False):
+    app_path  = os.path.dirname(sys.executable) # .exe
+else:
+    app_path = os.path.dirname(os.path.abspath(__file__)) # .py file
+
+now = datetime.now() # get date
+month_day_year = now.strftime("%m%d%y") # change date to string 
 
 website = "https://www.skysports.com/football"
 path = "/home/anas/Downloads/chromedriver-linux64/chromedriver"
@@ -21,11 +32,13 @@ titles = []
 
 
 for result in results: #store each data
-    title = result.find_element(by="xpath", value='//a[@class="sdc-site-tile__headline-link"]').get_attribute("href")
+    title = result.get_attribute("href")
     titles.append(title)
    
 my_dict = {'title': titles}
 headlines = pd.DataFrame(my_dict)
-headlines.to_csv('headline_headless.csv')
+
+result_file = os.path.join(app_path, f'headline-{month_day_year}.csv') # concatenate 2 paths together
+headlines.to_csv(result_file)
 
 driver.quit()
