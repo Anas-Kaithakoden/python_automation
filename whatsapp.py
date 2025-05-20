abaa6241915f71d8b28f3e0f2588a241696d8c10
Anas-kaithakoden <anaskaithakoden@gmail.com>
2025-05-20 22:19:22 +0530

    added group message option too

--- whatsapp.py
+++ whatsapp.py
@@ -3,5 +3,9 @@ import pywhatkit
 phone_number = input("Enter phone number: ")
 message = input("Message: ")
 
+# to a number
+pywhatkit.sendwhatmsg(phone_number, message, 10, 20) # hour , minute
 
-pywhatkit.sendwhatmsg(phone_number, message, 10, 20)
+# to a group
+group_link = input("Group_link: ")
+pywhatkit.sendwhatmsg_to_group(group_link, message, 10, 20)
