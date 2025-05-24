b16e47d3d2408dfc40afc8c41e14955cf7c7d076
Anas-kaithakoden <anaskaithakoden@gmail.com>
2025-05-24 14:32:56 +0530

    used undetecteable chrome driver

--- selenium_pro/cookie_clicker.py
+++ selenium_pro/cookie_clicker.py
@@ -1,16 +1,24 @@
+import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 import time
 
 website = "https://orteil.dashnet.org/cookieclicker/"
 path = "/home/anas/Downloads/chromedriver-linux64/chromedriver"
 
-service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service)
+#undetected chrome driver
+options = uc.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+
+driver = uc.Chrome(options=options)
+
 driver.get(website)
 
 #select a language
@@ -31,20 +39,25 @@ time.sleep(10)
 product_prefix = "productPrice"
 product = "product"
 
+
+
 while True:
     cookie.click()
     cookies_count = driver.find_element(By.ID, "cookies").text.split(" ")[0]
     cookies_count = int(cookies_count.replace(",", ""))
+    print(f"cookies count: {cookies_count}")
     #helper
     for i in range(4):
         price = driver.find_element(By.ID, product_prefix + str(i)).text
-        price = int(price.replace(",", ""))
+        print(f"price: {price}")
+        if not price:
+            continue
+        price = int(price.replace(",", ""))  
         if cookies_count >= price:
             driver.find_element(By.ID, product + str(i)).click()
+            print(f"Helper {i} brought")
+        else:
+            continue
 
 
 
-
-
-
-time.sleep(10)
